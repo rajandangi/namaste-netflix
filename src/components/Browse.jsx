5ed27b9ac29eb049c981e4bd5235725eba238823
rajandangi <rajandangi649@gmail.com>
2025-02-28 23:20:08 +1030

    feat: enhance Browse component with movie hooks and additional containers; add scrollbar utilities to CSS

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,9 +1,20 @@
-import Header from "@/components/Header"
+import Header from "@/components/Header";
+import MainContainer from "@/components/MainContainer";
+import SecondaryContainer from "@/components/SecondaryContainer";
+import useNowPlayingMovies from "@/hooks/useNowPlayingMovies";
+import usePopularMovies from "@/hooks/usePopularMovies";
+import useTopRatedMovies from "@/hooks/useTopRatedMovies";
 
 const Browse = () => {
+  useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   )
 }
